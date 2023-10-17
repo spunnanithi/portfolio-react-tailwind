8e60a69bfbd49d84975db329162ab19445769e14
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-10-16 19:09:12 -0700

    changed download pdf button to view pdf button

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -1,5 +1,5 @@
 // import useThemeSwitcher from "../../hooks/useThemeSwitcher";
-import { FiArrowDownCircle } from "react-icons/fi";
+import { FiFileText } from "react-icons/fi";
 // import developerLight from "../../images/developer.svg";
 // import developerDark from "../../images/developer-dark.svg";
 import { motion } from "framer-motion";
@@ -63,13 +63,13 @@ const AppBanner = () => {
 					}}
 					className="flex justify-center sm:block">
 					<a
-						download="SirasitPunnanithi.pdf"
-						href="/files/SirasitPunnanithi.pdf"
+						href="/files/Sirasit_Punnanithi_Software_Engineer_Resume.pdf"
+						target="_blank"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-secondary-blue hover:bg-primary-blue text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume">
-						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
+						<FiFileText className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiFileText>
 						<span className="text-sm sm:text-lg font-general-medium duration-100">
-							Download CV
+							My Resume
 						</span>
 					</a>
 				</motion.div>
